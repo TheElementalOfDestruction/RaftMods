56879ae3f24e60dd4c8ab6c46bfafd922898435b
TheElementalOfDestruction <arceusthe@gmail.com>
2022-08-04 15:59:12 -0700

    Updated build.py to allow for mods to have internal folders

--- build.py
+++ build.py
@@ -16,17 +16,18 @@ def main():
     for mod in mods:
         settings = mods[mod]
         print(f"Building {mod}");
+        # This is used to allow us to cut out the path of the path that is
+        # important for the name.
+        pathLen = len(mod.parts)
         with zipfile.ZipFile(f'build/{settings["slug"]}.rmod', 'w', zipfile.ZIP_DEFLATED, compresslevel = 4) as zf:
             # Go through each file for the mod.
-            for modFileName in mod.glob('*'):
-                # Skip the build settings.
-                if modFileName.name != 'build_settings.json':
-                    # Open the file on the disk.
-                    with open(modFileName, 'rb') as mf:
-                        # Open the file in the zip file.
-                        with zf.open(modFileName.name, 'w') as zmf:
-                            # Copy the data.
-                            zmf.write(mf.read())
+            for modFileName in mod.glob('**/*'):
+                # Skip non-files and the build settings.
+                if modFileName.is_file() and modFileName.name != 'build_settings.json':
+                    # Open the file in the zip file.
+                    with zf.open('/'.join(modFileName.parts[pathLen:]), 'w') as zmf:
+                        # Copy the data.
+                        zmf.write(modFileName.read_bytes())
 
     # Create the build folder.
     os.makedirs('build', exist_ok = True)
